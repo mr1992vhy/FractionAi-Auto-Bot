46d4fff7a89563bf109914745332defd306d8eed
mr1992vhy <58392399+mr1992vhy@users.noreply.github.com>
2025-01-26 21:07:49 +0330

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,70 @@
-# FractionAi-Auto-Bot
+## FRACTION AI AUTO BOT
+
+**Fraction AI Auto Bot**
+
+Potential Network : ETH Sepolia
+
+**Register** [Here](https://dapp.fractionai.xyz?referral=1B46649B)
+- Connect Wallet (New / Burner)
+- Go to "Dashboard"
+- Go to "My Agent" Tab
+- Click "+ Create New Agent"
+- Fill Starting Balance
+- Fill the System Prompt (You can Generate With AI)
+- Go to "My Agents"
+- Click "Enable Automation"
+- Done!
+
+Buy $ETH on Sepolia if need so much ETH Sepolia: [Here](https://testnetbridge.com/sepolia)
+
+You'll need a significant amount of $ETH on Sepolia, as this testnet involves battles that depend on your $ETH balance.
+
+## PREREQUISITE
+
+- Git
+- Node JS
+- Wallet Funded with Eth Sepolia
+
+## BOT FEATURE
+
+- Multi Account 
+- Support PK
+- Proxy Support
+- Agent Matchmarking Automation 
+
+
+## SETUP & CONFIGURE BOT
+
+### LINUX
+1. Clone project repository
+   ```
+   git clone https://github.com/airdropinsiders/FractionAi-Auto-Bot.git && cd FractionAi-Auto-Bot
+   ```
+2. Install Dependencies & Setup Accounts
+   ```
+   npm install && npm run setup
+   ```
+3. Configure your accounts
+   ```
+   nano accounts/accounts.js
+   ```
+4. Configure your bot config
+   ```
+   nano config/config.js
+   ```
+5. Configure your proxy
+   ```
+   nano config/proxy_list.js
+   ```
+6. To start the app run
+   ```
+   npm run start
+   ```
+
+## NOTE
+
+DWYOR & Always use a new wallet when running the bot, I am not responsible for any loss of assets.
+
+## LICENSE
+
+This project is licensed under the MIT License. See the [LICENSE](LICENSE) file for more details.
---
+++ app/index.js
@@ -0,0 +1,67 @@
+import { privateKey } from './accounts/accounts.js';
+import _0x1d5b89 from './src/core/core.js';
+import { Helper } from './src/utils/helper.js';
+import _0x48b9c7 from './src/utils/logger.js';
+import _0x3596ce from './src/utils/twist.js';
+async function operation(_0x422999) {
+  const _0x13b4a6 = new _0x1d5b89(_0x422999);
+  try {
+    await _0x13b4a6.connectWallet();
+    await _0x13b4a6.getBalance();
+    await _0x13b4a6.signIn();
+    await _0x13b4a6.getUserAgent();
+    if (_0x13b4a6.agentList.length == 0x0) {
+      await Helper.delay(0xea60, _0x422999, "Account " + _0x13b4a6.address + " Don't Have AI Agent Created, Please Create at Least 1 AI Agent With Automation OFF", _0x13b4a6);
+    }
+    for (const _0x578e7b of _0x13b4a6.agentList) {
+      if (_0x578e7b.automationEnabled == false) {
+        await _0x13b4a6.matchmarking(_0x578e7b);
+      }
+    }
+    await Helper.delay(0x3a980, _0x422999, "Account " + (privateKey.indexOf(_0x422999) + 0x1) + " Processing Done, Delaying for " + Helper.msToTime(0x3a980) + " until Next Matchmarking", _0x13b4a6);
+    await operation(_0x422999);
+  } catch (_0x4acc1c) {
+    if (_0x4acc1c.message) {
+      await Helper.delay(0x2710, _0x422999, "Error : " + _0x4acc1c.message + ", Retry again after 10 Second", _0x13b4a6);
+    } else {
+      await Helper.delay(0x2710, _0x422999, "Error :" + JSON.stringify(_0x4acc1c) + ", Retry again after 10 Second", _0x13b4a6);
+    }
+    await operation(_0x422999);
+  }
+}
+async function startBot() {
+  return new Promise(async (_0x5884c6, _0x482201) => {
+    try {
+      _0x48b9c7.info("BOT STARTED");
+      if (privateKey.length == 0x0) {
+        throw Error("Please input your account first on accounts.js file");
+      }
+      const _0x40d14f = [];
+      for (const _0x144be6 of privateKey) {
+        _0x40d14f.push(operation(_0x144be6));
+      }
+      await Promise.all(_0x40d14f);
+      _0x5884c6();
+    } catch (_0x380c26) {
+      _0x48b9c7.info("BOT STOPPED");
+      _0x48b9c7.error(JSON.stringify(_0x380c26));
+      _0x482201(_0x380c26);
+    }
+  });
+}
+(async () => {
+  try {
+    _0x48b9c7.clear();
+    _0x48b9c7.info('');
+    _0x48b9c7.info("Application Started");
+    console.log();
+    console.log(Helper.botName);
+    console.log("Join Us : https://t.me/AirdropInsiderID");
+    await startBot();
+  } catch (_0x41f961) {
+    _0x3596ce.clear();
+    _0x3596ce.clearInfo();
+    console.log("Error During executing bot", _0x41f961);
+    await startBot();
+  }
+})();
